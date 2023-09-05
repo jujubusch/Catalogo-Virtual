ceffa23c846262ed4402df1f49a764f03990c115
jujubusch <buschanajulia@gmail.com>
2023-09-05 16:25:55 -0300

    Finalização da Pagina de Login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-function App(props) {
+import { Avatar, Button } from "@mui/material";
+
+function App() {
   return (
     <>
+    <h1>Home</h1>
+    <Button variant="contained">Contained</Button>
+    <Avatar alt="Ana Busch" src="/static/images/avatar/3.jpg" />
     </>
   );
 }
+
 export default App;
